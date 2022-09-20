04a38091a1ad9b1eedb041d0b8dd2f7c750bf651
SirFerr <ursusmartimus007@gmail.com>
2022-09-20 10:13:06 +0300

    3

--- src/main/java/com/example/universityproject/Controller.java
+++ src/main/java/com/example/universityproject/Controller.java
@@ -1,14 +1,63 @@
 package com.example.universityproject;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    ObservableList<String> items;
     @FXML
-    private Label welcomeText;
+    private ComboBox<?> instituteName;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ComboBox<?> practiceName;
+
+    @FXML
+    private ComboBox<?> departmentName;
+
+    @FXML
+    private TextField position;
+
+    @FXML
+    private TextField orderName;
+
+    @FXML
+    private TextField supervisorFN;
+
+    @FXML
+    private TextField headOfDFN;
+
+    @FXML
+    private DatePicker orderDate;
+
+    @FXML
+    private DatePicker currentDate;
+
+    @FXML
+    private TextField practicePlaceAndTime;
+
+    @FXML
+    private TextField groupName;
+
+    @FXML
+    private ChoiceBox<?> courseNum;
+
+    @FXML
+    private ComboBox<?> directionName;
+
+    @FXML
+    private TextField profileName;
+
+    @FXML
+    private Button btnFileChooser;
+
+    @FXML
+    private Button btnSubmit;
+
 }
