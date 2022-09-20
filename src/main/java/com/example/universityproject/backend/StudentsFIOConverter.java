498bee4292c8c06485564bfa7654c83dd79b5147
TheJadeRabb1t <nukutac@gmail.com>
2022-09-20 23:10:32 +0300

    Code shorted, file creation almost done >(^_^)<

--- src/main/java/com/example/universityproject/backend/StudentsFIOConverter.java
+++ src/main/java/com/example/universityproject/backend/StudentsFIOConverter.java
@@ -1,15 +1,11 @@
 package com.example.universityproject.backend;
 
-
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class    StudentsFIOConverter {
-
     public static List<String> cutStud(List<String> students){
         List<String> newStudentsFormat = new ArrayList<>();
         for (String newFIOFormat:students) {
@@ -19,9 +15,7 @@ public class    StudentsFIOConverter {
                 latter.append(m.group() +".");
             }
            newStudentsFormat.add(newFIOFormat.replaceAll("\\s\\W+", String.valueOf(latter)));
-
         }
         return newStudentsFormat;
     }
-
 }
