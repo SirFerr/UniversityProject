1efc2624fb5b983ade45db50923f8ccb33c9e604
SirFerr <ursusmartimus007@gmail.com>
2022-09-20 17:56:53 +0300

    controller update 2

--- src/main/java/com/example/universityproject/frontend/Controller.java
+++ src/main/java/com/example/universityproject/frontend/Controller.java
@@ -2,10 +2,15 @@ package com.example.universityproject.frontend;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -71,6 +76,15 @@ public class Controller implements Initializable {
         practiceName.getItems().addAll(practiceNameArr);
         departmentName.getItems().addAll(departmentNameArr);
         courseNum.getItems().addAll(courseNumArr);
-        }
 
-    }
+        }
+    public void start() {
+        btnSubmit.setOnAction(e->{
+            submitText.setText("Запущен процесс создания файлов");
+            instituteName.getValue();//получение текущего значения из ComboBox
+            position.getText();//получение текущего значения из TextField
+            currentDate.getValue();//получение текущего значения из DatePicker
+            submitText.setText("Файлы созданы");
+    });
+        }
+}
