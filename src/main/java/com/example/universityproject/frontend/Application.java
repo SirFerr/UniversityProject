4ed9cf333f2264194bc25b316d841bdfb27e5570
SirFerr <ursusmartimus007@gmail.com>
2022-09-21 13:14:11 +0300

    fix

--- src/main/java/com/example/universityproject/frontend/Application.java
+++ src/main/java/com/example/universityproject/frontend/Application.java
@@ -10,8 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("applicationGUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 850);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("UP");
         stage.setScene(scene);
         stage.show();
     }
