dfaddf9879bca3602386eb348d5279dbec21b739
narko <nekit.2280.3390@gmail.com>
2022-09-22 23:21:11 +0300

    Добавлено: selenium api и jsoup api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,14 @@
 //
 
 module com.example.universityproject {
+    requires org.seleniumhq.selenium.api;
+    requires org.seleniumhq.selenium.ie_driver;
+    requires org.seleniumhq.selenium.devtools_v104;
+    requires org.seleniumhq.selenium.java;
+    requires org.seleniumhq.selenium.http;
+    requires io.github.bonigarcia.webdrivermanager;
     requires javafx.controls;
+    requires org.slf4j;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
@@ -15,6 +22,8 @@ module com.example.universityproject {
     requires org.controlsfx.controls;
     requires validatorfx;
     requires org.kordamp.bootstrapfx.core;
+
+
 
     exports com.example.universityproject;
 
