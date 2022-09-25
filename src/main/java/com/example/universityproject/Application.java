47195fd2df9d7eb07894b1a37d404cf4a6388ad5
Timofey <sitiev174@gmail.com>
2022-09-25 20:15:22 +0300

    Change universities

--- src/main/java/com/example/universityproject/Application.java
+++ src/main/java/com/example/universityproject/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("applicationGUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 850);
-        stage.setTitle("Hello!");
+        stage.setTitle("University Project");
         stage.setScene(scene);
         stage.show();
     }
